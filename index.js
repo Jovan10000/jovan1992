cda2814b4362080248ad5271bc0a5b4c225f8eae
Jovan10000 <vanjo201@gmail.com>
2023-10-11 14:08:12 +0200

    novi

---
+++ index.js
@@ -0,0 +1,11 @@
+var string = "string"
+var booolean = false;
+var number = 5
+var undefined;
+var nulll = null
+
+console.log(typeof string)
+console.log(typeof booolean)
+console.log(typeof number)
+console.log(typeof undefined)
+console.log(typeof nulll)
