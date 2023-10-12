aa86c8f5844f54e6c108b7be39b8287f01e4d17f
Jovan10000 <vanjo201@gmail.com>
2023-10-12 14:03:22 +0200

    dan4

---
+++ dan4.js
@@ -0,0 +1,35 @@
+var kg = 1.2
+
+var b = 1.2 / 0.1
+
+console.log(b)
+var bit = "cetvrti dan na BITU"
+
+console.log(3+3 + (2*4) - 5)
+
+var calculateminutes = 5
+var calculateseconds = 60
+var resuult = calculateminutes * calculateseconds
+
+console.log(resuult)
+
+var megabites = 1024 
+var gigbites = 4 
+var final = gigbites * megabites
+
+console.log(final)
+
+var a = 50 
+ a *= 3
+ console.log(a)
+ var b = 36 
+ b += 25
+
+
+ var c = 100
+ c /= 10
+
+ var d = 120 
+ d -= 60
+
+ console.log(b,c,d)
---
+++ dan4opet.js
@@ -0,0 +1,40 @@
+var article = 4
+var kb = 98
+
+var result = kb / article
+console.log(result)
+
+var ___ime = "Jovan"
+var $$godine = 31
+var opis5P = "no cat"
+
+console.log(typeof ___ime)
+console.log(typeof $$godine)
+console.log(typeof opis5P)
+
+var p = true
+
+
+console.log(p && p)
+
+var b = false
+
+console.log(p && b)
+
+var speed = 110 
+if (speed >= 120) {
+    console.log("you are to fast not safe slow down")
+
+} else if (speed < 120 && speed >= 60) {
+    console.log("your speed is just fine keep up")
+}
+
+var age = "a" 
+
+if (age <= 120) {
+    console.log("good job")
+} else if  (typeof age == "string") {
+    console.log("insert correct value")
+} else {
+    console.log("to big number")
+}
