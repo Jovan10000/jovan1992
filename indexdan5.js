1e0fbda2dc675635139aa8da4811d0dc306698e6
Jovan10000 <vanjo201@gmail.com>
2023-10-13 14:07:41 +0200

    novi

---
+++ indexdan5.js
@@ -0,0 +1,94 @@
+var a = 15 
+if (a % 5 == 0) {
+    console.log("number 5")
+} 
+else if (a % 3 == 0) {
+    console.log("number 3")
+}
+console.log(a)
+
+var b = 22
+
+if (typeof a == "number" && a % 2) {
+    console.log(a)
+}
+
+var c = 1 
+var  s = 2 
+var r = 3
+var h = -4 
+var p = 5
+if (c > s && c > s && c > r && c > h && c > p) {
+    console.log(a)
+}
+else if (s > c && s > r && s > h && s > p) {
+    console.log(s)
+}
+else if (h > c && h > p && h > r && h > s) {
+    console.log(h)
+}
+else if (p > h && p > r && p > c && p > s) {
+    console.log(p)
+}
+else {
+    console.log(r)
+}
+
+var result = s * r * h 
+
+if (result => 0 ) {
+    console.log("+")
+} else {e
+    console.log("-")
+}
+var q = 1
+var w = 2
+var e = 3
+
+if ( q > w && q > e) {
+    if (w > e) console.log(q, w , e )
+    else if (e > w) {
+} console.log(q ,e , w )
+}
+if (w > q && w > e) {
+    if (q > e ) {
+        console.log(w, q , e)
+    } else {
+        console.log(w,e ,q )
+    }
+}
+if (e > w && e > q) {
+    if (w > q) console.log(e, w , q)
+    else {
+        console.log(e,q ,w)
+}
+    }
+
+
+    var c = 60 
+    var f = (9* c / 5 ) + 32
+console.log(f)
+
+var far = (5* f - 160 ) / 9 
+console.log(far)
+
+var givenNum = 11
+var result = givenNum - 13
+if (result > 13) {
+    console.log(result * 2 )
+}
+else {
+    console.log(Math.abs(result))
+}
+
+var a = 25
+var b = 25
+if (a === 50 || b === 50 || a + b == 50) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+switch (day) {
+    case 
